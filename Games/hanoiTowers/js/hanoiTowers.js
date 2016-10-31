1a9099f351f2c7cdbda73bcb15d47111f80f56d7
toddlab <toddlab@gmail.com>
2016-10-31 13:40:39 -0600

    Update hanoiTowers.js

--- Games/hanoiTowers/js/hanoiTowers.js
+++ Games/hanoiTowers/js/hanoiTowers.js
@@ -1,1 +1,26 @@
+var numDiscs = 0;
 
+function disc (id) {
+	this.width = "";
+	this.peg = "left";
+	this.id = id || "";
+}
+
+function createCSS(newCss) {
+	var style = document.createElement('style');
+	style.type = 'text/css';
+	style.innerHTML = newCss;
+	document.getElementsByTagName('head')[0].appendChild(style);
+	document.getElementById('someElementId').className = 'cssClass';
+}
+
+function askForNumDiscs() {
+	numDiscs = prompt("How many discs do you want?", "0");
+}
+
+function initiate(){
+	askForNumDiscs();
+	for (a = 0; a < numDiscs; a++) {
+		disc(a);
+	}
+}
